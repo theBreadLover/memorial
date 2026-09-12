b636b49b14b7e3cd2030624699e41401d7f3ebf7
Emmanuel Garcia <garciatrejosemmanuel@outlook.com>
2026-09-12 10:11:33 -0400

    Implementing Tailwind Css to project

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,12 @@ import { NavLink } from 'react-router-dom';
 
 export default function Header() {
 	return (
-		<header	className="header-container">
-			<nav className="header-nav" aria-label="Main navigation">
+		<header className="sticky top-0 my-2 bg-white/10 backdrop-blur-md w-full">
+			<nav className="pt-1 pr-5 flex justify-end" aria-label="Main navigation">
 				<NavLink to="/" end className="nav-link">Home</NavLink>
 				<NavLink to="gallery" className="nav-link">Gallery</NavLink>
 			</nav>
 		</header>
+
 	)
 }
