02ecb1e0c0e3fcde0a58adc0a55bda516c8d6b4a
Emmanuel Garcia <garciatrejosemmanuel@outlook.com>
2026-09-12 10:09:11 -0400

    Addded ID's to each image

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -3,9 +3,9 @@ import images from "../utils/images.ts";
 
 export default function Gallery() {
 	return (
-		<div className="gallery-container">
+		<div className="bg-contrast-color">
 			{images.map(image => (
-				<PhotoCard imgSrc={image.src} caption={image.quote} />
+				<PhotoCard imgSrc={image.src} caption={image.quote} key={image.id}/>
 			))}
 		</div>
 	)
--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,35 +1,43 @@
 const images = [
 	{
 		src: "/images/image1.jpg",
-		quote: "test text for image 1."
+		quote: "test text for image 1.",
+		id: "image_1"
 	},
 	{
 		src: "/images/image2.jpg",
-		quote: "test text for image 2."
+		quote: "test text for image 2.",
+		id: "image_2"
 	},
 	{
 		src: "/images/image3.jpg",
-		quote: "test text for image 3."
+		quote: "test text for image 3.",
+		id: "image_3"
 	},
 	{
 		src: "/images/image4.jpg",
-		quote: "test text for image 4."
+		quote: "test text for image 4.",
+		id: "image_4"
 	},
 	{
 		src: "/images/image5.jpg",
-		quote: "test text for image 5"
+		quote: "test text for image 5",
+		id: "image_5"
 	},
 	{
 		src: "/images/image6.jpg",
-		quote: "test text for image 6"
+		quote: "test text for image 6",
+		id: "image_6"
 	},
 	{
 		src: "/images/image7.jpg",
-		quote: "test text for image 7"
+		quote: "test text for image 7",
+		id: "image_7"
 	},
 	{
 		src: "/images/image8.jpg",
-		quote: "test text for image 8"
+		quote: "test text for image 8",
+		id: "image_8"
 	},
 ]
 
