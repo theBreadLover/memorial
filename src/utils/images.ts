dd212c78ba3272429892bdcecb38c65c91c0cc81
Emmanuel Garcia <garciatrejosemmanuel@outlook.com>
2026-09-16 22:40:22 -0400

    modified path in images.ts

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,41 +1,43 @@
+const path: string = 'memorial/images';
+
 const images = [
 	{
-		src: "/images/image1.jpg",
+		src: path + "//image1.jpg",
 		quote: "test text for image 1.",
 		id: "image_1"
 	},
 	{
-		src: "/images/image2.jpg",
+		src: path + "//image2.jpg",
 		quote: "test text for image 2.",
 		id: "image_2"
 	},
 	{
-		src: "/images/image3.jpg",
+		src: path + "//image3.jpg",
 		quote: "test text for image 3.",
 		id: "image_3"
 	},
 	{
-		src: "/images/image4.jpg",
+		src: path + "//image4.jpg",
 		quote: "test text for image 4.",
 		id: "image_4"
 	},
 	{
-		src: "/images/image5.jpg",
+		src: path + "//image5.jpg",
 		quote: "test text for image 5",
 		id: "image_5"
 	},
 	{
-		src: "/images/image6.jpg",
+		src: path + "//image6.jpg",
 		quote: "test text for image 6",
 		id: "image_6"
 	},
 	{
-		src: "/images/image7.jpg",
+		src: path + "//image7.jpg",
 		quote: "test text for image 7",
 		id: "image_7"
 	},
 	{
-		src: "/images/image8.jpg",
+		src: path + "//image8.jpg",
 		quote: "test text for image 8",
 		id: "image_8"
 	},
